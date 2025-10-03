126bc28a7714ba37f215366aa60735551bd69cb1
Paracetamol <paracetamol.C8H9NO2@icloud.com>
2025-10-03 13:56:25 -0400

    test

--- src/test_CCoinBox.py
+++ src/test_CCoinBox.py
@@ -6,6 +6,15 @@ class Test_CCoinBox(unittest.TestCase):
     def test_pass(self):
         pass
 
+    def test_autre(self):
+        coinBox = CCoinBox()
+        coinBox.ajouter_25c()
+        coinBox.ajouter_25c()
+        coinBox.ajouter_25c()
+        coinBox.ajouter_25c()
+        coinBox.vente()
+        self.assertEqual(coinBox.get_monnaie_courante(), 2)
+
     def test_monnaie(self):
         coinBox = CCoinBox()
         coinBox.ajouter_25c()
